821213abf1d61fdb69492ad478da7dd23f3cc972
RodrigoRangel06 <rotore1717@hotmail.com>
2022-07-23 10:59:07 -0500

    Se actualiza el proyecto

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-//app.use(router)
+
 
 router(app)
-
-
 
 
 app.use('/app', express.static('public'))
 app.listen(3000);
 console.log('La aplicación está funcionando en http://localhost:3000');
 
-
-//QUEDÉ POR EL VIDEO 4 DE LA CARPETA 3
